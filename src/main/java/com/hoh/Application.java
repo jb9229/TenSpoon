df176304c588727e3bd4ae115d14f0b64a0cb7e8
jb9229 <jb9229@gamil.com>
2015-11-10 22:24:20 +0900

    회원가입 API 구현 중   - aws 배포 진행 중

--- src/main/java/com/hoh/Application.java
+++ src/main/java/com/hoh/Application.java
@@ -3,6 +3,8 @@ package com.hoh;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,7 +13,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  * Created by test on 2015-10-29.
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer{
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources(Application.class);
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
